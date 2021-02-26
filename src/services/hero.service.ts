ca4435389068d67c2367b6f5aa3f24b683a4603c
heinsc <heinsc@t-online.de>
2021-02-26 16:18:21 +0100

    1 file modified

--- src/services/hero.service.ts
+++ src/services/hero.service.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from '../app/hero';
 import { HEROES } from '../app/mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { 
+    this.messageService.add("HeroService initialized.");
+  }
 
   getHeroes(): Observable<Hero[]> {
     const tempHeroes = of(HEROES);
+    this.messageService.add("List of heroes requested.")
     return tempHeroes;
   }
 }
