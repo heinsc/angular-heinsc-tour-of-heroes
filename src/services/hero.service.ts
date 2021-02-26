3515c6b79df5c9c8a605a48db62a0ef8c14af3fb
heinsc <heinsc@t-online.de>
2021-02-26 17:33:41 +0100

    1 file modified

--- src/services/hero.service.ts
+++ src/services/hero.service.ts
@@ -3,18 +3,23 @@ import { Observable, of } from 'rxjs';
 import { Hero } from '../app/hero';
 import { HEROES } from '../app/mock-heroes';
 import { MessageService } from './message.service';
+import  { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'localhost:3000/heroes';  // URL to web api
 
-  constructor(private messageService: MessageService) { 
+  constructor(
+    private messageService: MessageService
+    , private httpClient: HttpClient
+  ) { 
     this.messageService.add("HeroService initialized.");
   }
 
   getHeroes(): Observable<Hero[]> {
-    const tempHeroes = of(HEROES);
+    const tempHeroes = this.httpClient.get<Hero[]>(this.heroesUrl);
     this.messageService.add("List of heroes requested.")
     return tempHeroes;
   }
