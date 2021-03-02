7311df1a8029a6437d02059af07fa56e4ad86925
heinsc <heinsc@t-online.de>
2021-03-02 10:42:43 +0100

    1 file modified

--- src/services/hero.service.ts
+++ src/services/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Hero } from '../app/hero';
 import { MessageService } from './message.service';
 import  { HttpClient} from '@angular/common/http';
@@ -19,11 +20,24 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     this.log("getHeroes() entered.");
-    const tempHeroes = this.httpClient.get<Hero[]>(this.heroesUrl);
+    const tempHeroes = this
+      .httpClient
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(
+          (error: any, caught: Observable<any>) => { 
+          	this.log(`getHeroes - error ${error}, $ {caught}`);
+          	return of([]);
+          }
+        )
+      );
     this.log("getHeroes() - Heroes found: " + tempHeroes);
     this.messageService.add("List of heroes requested.")
     return tempHeroes;
   }
+handleError<T>(arg0: string,arg1: undefined[]): (err: any,caught: Observable<any>) => import("rxjs").ObservableInput<any> {
+throw new Error('Method not implemented.');
+}
 
   getHero(id): Observable<Hero[]> {
     const url = `${this.heroesUrl}/?id=${id}`;
