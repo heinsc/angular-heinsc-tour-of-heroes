410991654114aef7209ff8f4dc640ce0ef30f85a
heinsc <heinsc@t-online.de>
2021-03-02 14:43:23 +0100

    1 file modified

--- src/services/hero.service.ts
+++ src/services/hero.service.ts
@@ -67,7 +67,9 @@ handleError<T>(operation = "operation", result?: T): (err: any,caught: Observabl
         )
       );
   }
-
+  updateHero(hero: Hero) {
+    throw new Error('Method not implemented.');
+  }
   log(message: String) {
     this.messageService.add(`HeroService: ${message}`);
     console.log(`HeroService: ${message}`);
