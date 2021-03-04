4f8fc86703c92b1d29e2bb8bbe47bb2db4ab378a
heinsc <heinsc@t-online.de>
2021-03-04 16:35:12 +0100

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { NewHeroComponent } from './new-hero/new-hero.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent }
