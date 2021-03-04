415f33775e4059e4fbde09e02f0fa489eee6ede7
heinsc <heinsc@t-online.de>
2021-03-04 10:59:09 +0000

    1 file modified

--- src/services/hero.service.ts
+++ src/services/hero.service.ts
@@ -72,6 +72,7 @@ handleError<T>(operation = "operation", result?: T)
       );
   }
   updateHero(hero: Hero) : Observable<Hero[]> { 
+    
     return this.httpClient.put(
       this.heroesUrl + '/', hero, this.httpOptions
     ).pipe(
